8da4c805d45fa93d303091cb8690271c788a8170
Jake <jake.t.bouma@gmail.com>
2022-03-20 17:40:33 +0000

    fct

--- models/marts/core/fct_orders.sql
+++ models/marts/core/fct_orders.sql
@@ -1,20 +1,20 @@
 with orders as (
     select * from {{ ref('stg_orders') }}
-)
+),
 
-with successful_payments as (
-    select * from {{ ref('payments') }}
+successful_payments as (
+    select * from {{ ref('stg_payments') }}
     where status = 'success'
-)
+),
 
 orders_with_payments as (
 
     select
         order_id,
         customer_id,
-        payments.amount as amount
+        successful_payments.amount as amount
     from orders
-    left join payments using order_id
+    left join successful_payments using (order_id)
 )
 
 select * from orders_with_payments
